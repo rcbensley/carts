9d082a20106d59174b30428186dfa87f58be71d0
rcbensley <richardbensley@gmail.com>
2022-04-25 22:33:26 +0100

    things

--- chief/main.lua
+++ chief/main.lua
@@ -3,50 +3,33 @@ height = 240
 
 briefs = {
 	-- Holo
-	{{1, "Chief!"}, {4, "Yes sir"}, {2, "Riker's blown his loa-leave, in the holodeck again"}, {6, "..."}, {3,"Be a dear and scrub the holo-filters"}}
-}	
+	{ { 1, "Chief!" }, { 4, "Yes sir" }, { 2, "Riker's blown his loa-leave, in the holodeck again" }, { 6, "..." }, { 3, "Be a dear and scrub the holo-filters" } }
+}
 
-
-function mHolo()
-end
-
-function mBirthday()
-end
 
 function love.load()
-bg = 0
-screen = 0
-briefIdx = 1
-mission = 1
-briefLen = #briefs[mission]
-timer = 0
-
-title = love.graphics.newImage("img/title1.png")
-blank = love.graphics.newImage("img/game.png")
-
-briefScreens = {
-	love.graphics.newImage("img/cpt_order.png"),
-	love.graphics.newImage("img/cpt_quiz.png"),
-	love.graphics.newImage("img/cpt_silly.png"),
-	love.graphics.newImage("img/ob_yes.png"),
-	love.graphics.newImage("img/ob_what.png"),
-	love.graphics.newImage("img/ob_gross.png"),
-}
-
-briefTexts = {
-	"Chief!",
-	"Miles!",
-	"O'Brien!",
-	"Sir?",
-	"Yes, sir?",
-	"Yes, dear?",
-	"...",
-	"What?",
-}
 
 
-briefImg = briefScreens[briefs[mission][briefIdx][1]]
-briefText = briefs[mission][briefIdx][2]
+	title = love.graphics.newImage("img/title1.png")
+	blank = love.graphics.newImage("img/game.png")
+
+	briefScreens = {
+		love.graphics.newImage("img/cpt_order.png"),
+		love.graphics.newImage("img/cpt_quiz.png"),
+		love.graphics.newImage("img/cpt_silly.png"),
+		love.graphics.newImage("img/ob_yes.png"),
+		love.graphics.newImage("img/ob_what.png"),
+		love.graphics.newImage("img/ob_gross.png"),
+	}
+
+	bg = 0
+	screen = 0
+	briefIdx = 1
+	mission = 1
+	briefLen = #briefs[mission]
+	timer = 0
+	briefImg = briefScreens[briefs[mission][briefIdx][1]]
+	briefText = briefs[mission][briefIdx][2]
 end
 
 function love.update(dt)
@@ -54,8 +37,8 @@ function love.update(dt)
 
 	if screen == 0 then
 		if timer >= 0.5 and love.keyboard.isDown("space") then
-		timer = 0
-		screen = 1
+			timer = 0
+			screen = 1
 		end
 	end
 
@@ -98,7 +81,7 @@ function love.draw()
 
 	-- Draw current brief
 	if screen == 1 then
-		love.graphics.draw(briefImg, 0 , 0)
+		love.graphics.draw(briefImg, 0, 0)
 		love.graphics.print(briefText, 10, 200)
 	end
 
