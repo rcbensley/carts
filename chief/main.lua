f56c1cac1cbacfb1e07c9ca01d9209b9df42e877
rcbensley <richardbensley@gmail.com>
2022-05-09 21:40:47 +0100

    pallette and game loops

--- chief/main.lua
+++ chief/main.lua
@@ -3,15 +3,21 @@ height = 240
 
 briefs = {
 	-- Holo
-	{ { 1, "Chief!" }, { 4, "Yes sir" }, { 2, "Riker's blown his loa-leave, in the holodeck again" }, { 6, "..." }, { 3, "Be a dear and scrub the holo-filters" } }
+	{ { 1, "Chief!" }, { 4, "Yes sir" }, { 2, "Riker's blown his loa-leave, in the holodeck again" }, { 6, "..." }, { 3, "Be a dear and scrub the holo-filters" } },
+	-- Birthday
+	{{1, "Chief!"} ,{1, "It's Commander Riker's Birthday"}, {4, "..."}, {2, "Isn't he a scamp?"}},
 }
 
 
+
+
 function love.load()
+	minigame = false
 	rx = 0.0
 	ry = 0.0
 	x = 0.0
 	y = 0.0
+	state = {}
 
 
 	title = love.graphics.newImage("img/title1.png")
@@ -26,14 +32,80 @@ function love.load()
 		love.graphics.newImage("img/ob_gross.png"),
 	}
 
+	gameScreens = {
+		love.graphics.newImage("img/ob_holo_hand.png"),
+		love.graphics.newImage("img/ob_nod1.png"),
+		love.graphics.newImage("img/ob_nod2.png"),
+	}
+
 	bg = 0
 	screen = 0
 	briefIdx = 1
 	mission = 1
+	missionCount = #briefs
 	briefLen = #briefs[mission]
 	timer = 0
 	briefImg = briefScreens[briefs[mission][briefIdx][1]]
 	briefText = briefs[mission][briefIdx][2]
+end
+
+function game_holo()
+	if love.keyboard.isDown("a", "left") then
+		rx = -1.0
+	end
+	if love.keyboard.isDown("d", "right") then
+		rx = 1.0
+	end
+	if love.keyboard.isDown("w", "up") then
+		ry = -1.0
+	end
+	if love.keyboard.isDown("s", "down") then
+		ry = 1.0
+	end
+	minigame = false
+end
+
+function draw_game_holo()
+	love.graphics.draw(gameScreens[1])
+end
+
+function game_birthday()
+	if #state == 0 then
+		state["img"] = 2
+		state["nods"] = 0
+	end
+	if love.keyboard.isDown("space") then
+		if state["img"] == 3 then
+			state["img"] = 2
+		else
+			state["img"] = 3
+		end
+		state["nods"] = state["nods"] + 1
+	end
+	if state["nods"] >= 7 then
+		minigame = false
+	end
+end
+
+function draw_game_birthday()
+	love.graphics.draw(gameScreens[state["img"]])
+end
+
+briefGames = {
+	{game_holo, draw_game_holo},
+	{game_birthday, draw_game_birthday}
+}
+
+function nextMission()
+	if mission >= missionCount then
+		mission = 1
+	else
+		mission = mission + 1
+	end
+	minigame = false
+	screen = 1
+	timer = 0
+	state = {}
 end
 
 function love.update(dt)
@@ -58,19 +130,22 @@ function love.update(dt)
 			if briefIdx > briefLen then
 				briefLen = #briefs[mission]
 				briefIdx = 1
+				minigame = true
 				screen = 2
 			end
 			briefImg = briefScreens[briefs[mission][briefIdx][1]]
 			briefText = briefs[mission][briefIdx][2]
-			print(screen .. mission .. briefIdx)
 		end
 	end
 
 	if screen == 2 then
+		if minigame then
+			briefGames[1][mission]()
+			return
+		end
 		if timer >= 1 and love.keyboard.isDown("space") then
 			print("game " .. mission .. " has ended")
-			screen = 1
-			timer = 0
+			nextMission()
 		end
 	end
 
@@ -91,7 +166,12 @@ function love.draw()
 
 	-- minigame
 	if screen == 2 then
-		love.graphics.draw(blank, 0, 0)
+		if minigame then
+			briefGames[2][mission]()
+			return
+		else
+			love.graphics.draw(blank, 0, 0)
+		end
 	end
 
 end
